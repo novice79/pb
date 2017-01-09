69b902fa298dfcf48cb33bd2ab733cb90bdb69fe
david <david@cninone.com>
2017-01-09 17:56:33 +0800

    remove space

--- install_sproxy.sh
+++ install_sproxy.sh
@@ -2,7 +2,7 @@
  
 apt-get install -y software-properties-common python-software-properties supervisor  \
     vim cron curl squid3 stunnel4 nghttp2 upstart-sysv \
-    && update-initramfs -u \    
+    && update-initramfs -u \
     && apt-get purge systemd -y \
     && apt-get clean && apt-get autoclean && apt-get remove
 mkdir -p /var/log/lep 
