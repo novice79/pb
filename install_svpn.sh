1ee92e2b1dd486fa6e67a94aedaeef00e3485c41
david <david@cninone.com>
2017-06-21 09:35:32 +0800

    use 'apt-get install letsencrypt' instead of clone repo

--- install_svpn.sh
+++ install_svpn.sh
@@ -3,7 +3,7 @@
 apt-get update && apt-get upgrade -y
 apt-get install build-essential -y
 apt-get install -y software-properties-common python-software-properties supervisor \
-    cron curl git iptables 
+    cron curl git iptables letsencrypt
 cd /vpn
 tar xzvf softether*.tar.gz
 
@@ -11,6 +11,6 @@ cd vpnserver && make i_read_and_agree_the_license_agreement
 chmod 600 *
 chmod 700 vpnserver
 chmod 700 vpncmd
-cd / && git clone https://github.com/letsencrypt/letsencrypt && letsencrypt/letsencrypt-auto --help
+
 
 exit 0
