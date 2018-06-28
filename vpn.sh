b7d0287b124a79950e3d7eaaee7c56dd35652f9f
novice <wei-jiang@163.com>
2018-06-28 14:46:18 +0800

    testing

--- vpn.sh
+++ vpn.sh
@@ -52,9 +52,8 @@ sudo mkdir -p /etc/nginx/conf.d/
 
 if ! docker ps | grep -q 'novice/pb'  ; then
     echo "starting vpn..."
-    sudo docker run -d -p 80:80 -p 443:443 -p 992:992 -p 5555:5555 -p 1194:1194 \
-    -p 500:500/udp -p 4500:4500/udp -p 1701:1701/udp -p 1194:1194/udp --name pb \
-    -p 1979:1979 -p 1982:1982 \
+    sudo docker run -d --name pb \
+    --network="host" \
     -v "/etc/letsencrypt:/etc/letsencrypt" \
     -v "/etc/nginx/conf.d:/etc/nginx/conf.d" \
     novice/pb:latest $@     
